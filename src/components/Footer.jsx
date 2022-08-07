e5ccbfd2a81a3fc62b01be7dee56d0dfcc24eb95
egyc0der <ahmed.pjt@hotmail.com>
2022-08-08 00:03:33 +0200

    responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -86,59 +86,61 @@ const Payment = styled.img`
 
 const Footer = () => {
   return (
-    <Container>
-      <Left>
-        <Logo>Click 4E-Business</Logo>
-        <Desc>
-          There are many variations of passages of Lorem Ipsum available, but
-          the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don’t look even slightly believable.
-        </Desc>
-        <SocialContainer>
-          <SocialIcon color="3B5999">
-            <Facebook />
-          </SocialIcon>
-          <SocialIcon color="E4405F">
-            <Instagram />
-          </SocialIcon>
-          <SocialIcon color="55ACEE">
-            <Twitter />
-          </SocialIcon>
-          <SocialIcon color="E60023">
-            <Pinterest />
-          </SocialIcon>
-        </SocialContainer>
-      </Left>
-      <Center>
-        <Title>Useful Links</Title>
-        <List>
-          <ListItem>Home</ListItem>
-          <ListItem>Cart</ListItem>
-          <ListItem>Man Fashion</ListItem>
-          <ListItem>Woman Fashion</ListItem>
-          <ListItem>Accessories</ListItem>
-          <ListItem>My Account</ListItem>
-          <ListItem>Order Tracking</ListItem>
-          <ListItem>Wishlist</ListItem>
-          <ListItem>Wishlist</ListItem>
-          <ListItem>Terms</ListItem>
-        </List>
-      </Center>
-      <Right>
-        <Title>Contact</Title>
-        <ContactItem>
-          <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
-        </ContactItem>
-        <ContactItem>
-          <Phone style={{marginRight:"10px"}}/> +1 234 56 78
-        </ContactItem>
-        <ContactItem>
-          <MailOutline style={{marginRight:"10px"}} /> contact@lama.dev
-        </ContactItem>
-        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
-      </Right>
-    </Container>
-  );
+		<Container>
+			<Left>
+				<Logo>Click 4E-Business</Logo>
+				<Desc>
+					There are many variations of passages of Lorem Ipsum available, but
+					the majority have suffered alteration in some form, by injected
+					humour, or randomised words which don’t look even slightly believable.
+				</Desc>
+				<SocialContainer>
+					<SocialIcon color='3B5999'>
+						<Facebook />
+					</SocialIcon>
+					<SocialIcon color='E4405F'>
+						<Instagram />
+					</SocialIcon>
+					<SocialIcon color='55ACEE'>
+						<Twitter />
+					</SocialIcon>
+					<SocialIcon color='E60023'>
+						<Pinterest />
+					</SocialIcon>
+				</SocialContainer>
+			</Left>
+			<Center>
+				<Title>Useful Links</Title>
+				<List>
+					<ListItem>Home</ListItem>
+					<ListItem>Cart</ListItem>
+					<ListItem>Man Fashion</ListItem>
+					<ListItem>Woman Fashion</ListItem>
+					<ListItem>Accessories</ListItem>
+					<ListItem>My Account</ListItem>
+					<ListItem>Order Tracking</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Wishlist</ListItem>
+					<ListItem>Terms</ListItem>
+				</List>
+			</Center>
+			<Right>
+				<Title>Contact</Title>
+				<ContactItem>
+					<Room style={{ marginRight: "10px" }} /> Lorem ipsum dolor sit amet,
+					consectetur adipisicing elit.
+				</ContactItem>
+				<ContactItem>
+					<Phone style={{ marginRight: "10px" }} /> +1 234 56 78
+				</ContactItem>
+				<ContactItem>
+					<MailOutline style={{ marginRight: "10px" }} />{" "}
+					click4ebusiness@gmail.com
+				</ContactItem>
+				<Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
+			</Right>
+		</Container>
+	);
 };
 
 export default Footer;
